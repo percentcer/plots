153fec2c61250334e6ff43e4edbca8c487aedc1d
Spencer Miller <spence.miller@gmail.com>
2022-11-18 21:59:42 -0800

    [lorenz] keep points in 3D, do projection at the end

--- lorenz.py
+++ lorenz.py
@@ -5,11 +5,13 @@
 # https://github.com/evil-mad/axidraw
 # http://axidraw.com/docs
 
+import numpy as np
+
 from common.page import PAGE_WIDTH, PAGE_HEIGHT, gen_border
 from common.svg import svg_preview
 
 # --- draw transforms ---------------------------------------------------------
-MAG = 3
+MAG = 4
 CX = PAGE_WIDTH / 2
 CY = PAGE_HEIGHT / 2
 SX = (PAGE_WIDTH / PAGE_HEIGHT) * MAG
@@ -24,18 +26,20 @@ A = 99
 B = 11
 C = 1
 
-
 def gen(p_x, p_y, p_z):
     COORDS = []
-    for _ in range(5000):
+    for _ in range(3000):
         p_x += (p_y - p_x) / B
         p_y += p_x - (p_x * p_z - p_y) / A
         p_z += p_x * p_y / p_z - C
-        COORDS.append([p_x, p_y])
+        COORDS.append([p_x, p_y, p_z])
     return COORDS
 
 # --- main --------------
-PATH = gen(9, 1, 1)
+PATH = np.array(gen(9, 1, 1))
+PATH = PATH - PATH[0]
+PATH = np.delete(PATH, 1, 1) # array, column [0,1,2], axis [0,1]
+PATH = PATH * [SX, SY] + [CX, CY]
 BORDER = gen_border(CX, CY, EXT_WIDTH, EXT_HEIGHT)
 
 # axi_draw_paths(BORDER, PATH)
