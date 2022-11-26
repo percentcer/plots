0d7bc8630d2612f189a1086104a8e624483ba578
Spencer Miller <spence.miller@gmail.com>
2022-11-26 11:29:07 -0800

    [math] add sign, zsign, SQRT2

--- common/math.py
+++ common/math.py
@@ -1,5 +1,15 @@
+import math
+
 # --- math --------------------------------------------------------------------
 TAU = 6.28
+SQRT2 = math.sqrt(2)
 
 def clamp(val, minval, maxval):
     return max(min(val, maxval),minval)
+
+def sign(val):
+    # considering zero positive
+    return -1 if val < 0 else 1
+
+def zsign(val):
+    return 0 if val == 0 else sign(val)
