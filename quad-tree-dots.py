763000c770d21016bb921d85ac20f6831354c62e
Spencer Miller <spence.miller@gmail.com>
2022-11-26 11:34:52 -0800

    [plot] draw quad tree leaves as circles of varying sizes

--- quad-tree-dots.py
+++ quad-tree-dots.py
@@ -7,13 +7,12 @@
 
 from collections import Counter
 import numpy as np
-import random
 
-import common.hexgrid as hg
 from common.axidraw import axi_draw_svg, axi_draw_paths
+from common.math import TAU, SQRT2
 from common.page import PAGE_WIDTH, PAGE_HEIGHT
+from common.quad_trees import quarter_space
 from common.svg import svg_circles, svg_doc, svg_write, svg_rects
-from common.math import clamp
 
 # --- draw transforms ---------------------------------------------------------
 CX = PAGE_WIDTH / 2
@@ -25,34 +24,36 @@ SY = 1
 EXT = 1024
 EXT_W = EXT
 EXT_H = EXT
+OFFS = np.array(
+    [
+        [1, 1],
+        [-1, 1],
+        [-1, -1],
+        [1, -1],
+    ]
+)
 
 
-def find_quadrant(point: hg.CPoint, center: hg.CPoint, ext: int):
-    #          ^
-    #    1     |     0
-    #          |
-    #          |
-    # ---------+--------->
-    #          |
-    #          |
-    #    2     |     3
-    #          |
-    pass
-
-
-def gen(n: int, depth: int, ext: int):
-    # pick n random positions
-    pos = [hg.CPoint(random.randint(-ext, ext), random.randint(-ext, ext))
-           for _ in range(n)]
+def gen(scl):
     # generate quad tree
-    # generate circles based on child count
-    # return circles
-    return pos
+    root = quarter_space(10, 7)
+    # walk the tree, each leaf should resolve in a circle (rad based on depth, position based on quad offsets)
+    def walk(node, depth, pos):
+        if not len(node):
+            return [(*(pos*scl+[CX,CY]), pow(0.5, depth-1) * SQRT2 * scl)]
+        return (
+              walk(node[0], depth + 1, pos + OFFS[0] * pow(0.5, depth) * SQRT2)
+            + walk(node[1], depth + 1, pos + OFFS[1] * pow(0.5, depth) * SQRT2)
+            + walk(node[2], depth + 1, pos + OFFS[2] * pow(0.5, depth) * SQRT2)
+            + walk(node[3], depth + 1, pos + OFFS[3] * pow(0.5, depth) * SQRT2)
+        )
+    return walk(root, 0, np.array([0,0]))
 
 
 # --- main --------------
-svgcircles = svg_circles(gen(5, 4, EXT_W))
-svgborder = svg_rects([CX-EXT_W, CY-EXT_H, EXT_W*2, EXT_H*2])
+circles = gen(48)
+svgcircles = svg_circles(*circles)
+svgborder = svg_rects([CX - EXT_W, CY - EXT_H, EXT_W * 2, EXT_H * 2])
 doc = svg_doc(*svgborder, *svgcircles)
 
 svg_write(doc)
