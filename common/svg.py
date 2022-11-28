18acd710fb3647794e6e19fed600776c39489063
Spencer Miller <spence.miller@gmail.com>
2022-11-28 13:41:25 -0800

    print out html preview when writing svg (for page setup preview)
    this avoids needing to put a rect in the svg, which has the undesirable consequence of drawing itself

--- common/svg.py
+++ common/svg.py
@@ -2,22 +2,38 @@ from common.page import PAGE_WIDTH, PAGE_HEIGHT, PAGE_MARGIN
 
 # --- svg preview -------------------------------------------------------------
 def svg_pointlist(points):
-    return ' '.join([f"{p[0]},{p[1]}" for p in points])
-    
+    return " ".join([f"{p[0]},{p[1]}" for p in points])
+
+
 def svg_margins():
-    return [f"""<rect fill="none" stroke="blue" stroke-width="0.2" width="{PAGE_WIDTH}" height="{PAGE_HEIGHT}"/>"""]
+    return [
+        f"""<rect fill="none" stroke="blue" stroke-width="0.2" width="{PAGE_WIDTH}" height="{PAGE_HEIGHT}"/>"""
+    ]
+
 
 def svg_rects(*specs):
-    return [f'<rect x="{r[0]}" y="{r[1]}" width="{r[2]}" height="{r[3]}" fill="none" stroke="black" stroke-width="0.2"/>' for r in specs]
+    return [
+        f'<rect x="{r[0]}" y="{r[1]}" width="{r[2]}" height="{r[3]}" fill="none" stroke="black" stroke-width="0.2"/>'
+        for r in specs
+    ]
+
 
 def svg_polylines(*paths):
-    return [f'<polyline points="{svg_pointlist(p)}" fill="none" stroke="black" stroke-width="0.2"/>' for p in paths]
+    return [
+        f'<polyline points="{svg_pointlist(p)}" fill="none" stroke="black" stroke-width="0.2"/>'
+        for p in paths
+    ]
+
 
 def svg_circles(*positions):
-    return [f'<circle cx="{p[0]}" cy="{p[1]}" r="{p[2]}" fill="none" stroke="black" stroke-width="0.2"/>' for p in positions]
+    return [
+        f'<circle cx="{p[0]}" cy="{p[1]}" r="{p[2]}" fill="none" stroke="black" stroke-width="0.2"/>'
+        for p in positions
+    ]
+
 
 def svg_doc(*elements):
-    text = '\n'.join(elements)
+    text = "\n".join(elements)
     return f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
         <svg
             xmlns:dc="http://purl.org/dc/elements/1.1/"
@@ -29,13 +45,17 @@ def svg_doc(*elements):
             viewBox="0 0 {PAGE_WIDTH + PAGE_MARGIN * 2} {PAGE_HEIGHT + PAGE_MARGIN * 2}"
             height="{PAGE_HEIGHT + PAGE_MARGIN * 2}mm"
             width="{PAGE_WIDTH + PAGE_MARGIN * 2}mm">
-        <rect width="100%" height="100%" fill="#f4f4f4"/>
         <g transform="translate({PAGE_MARGIN},{PAGE_MARGIN})">
             {text}
         </g>
         </svg>
         """
 
-def svg_write(doc, filename='preview.svg'):
-    with open(filename,'w') as fsvg:
+
+def svg_write(doc, filename="preview"):
+    with open(f"{filename}.svg", "w") as fsvg:
         fsvg.write(doc)
+    with open(f"{filename}.html", "w") as hsvg:
+        hsvg.write(
+            f'<!DOCTYPE html><html><body style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%)"><div style="background:#fafafa;border:solid 1px #e0e0e0">{doc}</div></body></html>'
+        )
