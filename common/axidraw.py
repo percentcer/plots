4bba93d8570d6b7080a0c84bc6fbe1b9610fd324
Spencer Miller <spence.miller@gmail.com>
2025-12-16 00:30:25 -0800

    enable hidden line removal

--- common/axidraw.py
+++ common/axidraw.py
@@ -35,6 +35,7 @@ def axi_draw_svg(input_svg):
     ad.options.pen_pos_down = 30
     ad.options.pen_pos_up = 60
     ad.options.random_start = True
+    ad.options.hiding = True # enable hidden line removal
 
     # ad.options.reordering = 2
     ad.plot_run(True)
